defe501924d9f2c4fc4dd28f989f126177938d26
Miftakhul Ma'arif <mmtaxcool9@gmail.com>
2013-01-11 23:15:13 +0700

    Miftah, Nanad, Revi, Ayik
    forum view 

--- application/views/user/forum.php
+++ application/views/user/forum.php
@@ -2,11 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>MI 3B Jos</title>
+<title>Forum | MI 3B Jos</title>
 <link href="<?=base_url();?>/style/css/templatemo_style.css" rel="stylesheet" type="text/css" />
-
 <link rel="stylesheet" type="text/css" href="<?=base_url();?>style/css/ddsmoothmenu.css" />
-
 <script type="text/javascript" src="<?=base_url();?>style/js/jquery.min.js"></script>
 <script type="text/javascript" src="<?=base_url();?>style/js/ddsmoothmenu.js">
 
@@ -17,7 +15,34 @@
 ***********************************************/
 
 </script>
-
+<script type="text/javascript"  src="<?php echo base_url() ?>style/js/tiny_mce/tiny_mce.js"></script>
+<script language="javascript"  type="text/javascript">
+      tinyMCE.init({
+        // General options
+        mode : "textareas",
+        theme : "advanced",
+        skin : "o2k7",
+        plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,inlinepopups,autosave",
+ 
+        // Theme options
+        theme_advanced_buttons1 : "bold,italic,underline,undo,redo,|,cut,copy,paste,pastetext,pasteword,|,search,replace",
+        theme_advanced_buttons2 : "emotions,iespell,media,advhr,|,print,|,ltr,rtl",
+        theme_advanced_buttons3 : "",
+        //theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        theme_advanced_resizing : true,
+        content_css : "css/word.css",
+        template_external_list_url : "lists/template_list.js",
+        external_link_list_url : "lists/link_list.js",
+        external_image_list_url : "lists/image_list.js",
+        media_external_list_url : "lists/media_list.js",
+ 
+         
+    });
+</script>
+ 
 <script type="text/javascript">
 
 ddsmoothmenu.init({
@@ -29,7 +54,14 @@ ddsmoothmenu.init({
 })
 
 </script>
-
+<script>
+$(function() {
+	
+	$( "#tabse" ).tabs();
+	
+	});
+	
+</script>
 
 <script language="javascript" type="text/javascript">
 	window.addEvents({
@@ -76,7 +108,12 @@ ddsmoothmenu.init({
     <div id="templatemo_main_top"></div>
     <div id="templatemo_main">
     	<?php include "user_template/sidebar.php"?>
-        
+		<a href="<?=base_url();?>index.php/ecommerce/forum">Home</a> > <a href="<?=base_url();?>index.php/ecommerce/forum">Forum</a>
+		<?php foreach($category->result_array() as $hh):?>
+        <?php if($this->uri->segment(3) == null){ ?>
+			
+		<?php }else{ ?> > <?=$hh['category']; } ?>
+        <?php endforeach;?>
         <div id="content" class="forum">
 		<div id="templatemo_search">
                 <form action="#" method="get">
@@ -84,9 +121,81 @@ ddsmoothmenu.init({
                   <input type="submit" name="Search" value="" alt="Search" id="searchbutton" title="Search" class="sub_btn"  />
                 </form>
             </div>
-			<br>
-        	<h2>Forum</h2>
-            
+			<br />
+			<?php			
+				
+				foreach($category->result_array() as $cat):
+				
+				?>
+				<br /><h3><a href="<?=base_url();?>index.php/ecommerce/forum/<?=$cat['id'];?>"><?php echo $cat['category'];?></a></h3>
+			<?php
+				if($this->uri->segment(3)== null){
+					echo "";
+				}else{
+			?>
+				<br /><h4><a href="<?=base_url();?>index.php/ecommerce/forum/new_thread/<?=$cat['id'];?>" class="thread"><? echo "Add Thread";?></a></h4>
+			<?php } 
+			?>
+				<blockquote>
+			<?php
+				echo "<h5>Last Thread</h5>";
+				foreach($thread->result_array() as $row):
+				$num = $row['id'];
+				$this->db->like('id_thread', $num);
+				$this->db->from('post');
+				$replies = $this->db->count_all_results();
+				
+				
+				$this->db->where('id', $num);
+				$this->db->from('thread');
+				$query = $this->db->get();
+				
+				if($cat['id']==$row['id_category']){	
+				
+			?>		
+					<h5>
+					<a href="#" id="expand"><img src="<?=base_url();?>/style/images/templatemo_list.png"></a>					
+					<a href="<?=base_url();?>index.php/ecommerce/thread/<?=$row['id'];?>">
+					<?php echo $row['title'];?>
+					</a>
+					</h5><br />
+					
+					<h6>Replies : <a href="<?=base_url();?>index.php/ecommerce/thread/<?=$row['id'];?>"><?php echo $replies; ?></a> | viewed : <?php foreach($query->result_array() as $viewed): echo $viewed['viewed']; endforeach;?> |<i> <?php echo $row['inserted_date'];?> </i>| by : <?php echo $row['inserted_by'];?></h6>
+					
+					<p>
+				
+			<?php 				
+				}
+				endforeach;
+				echo "</blockquote>";
+
+				endforeach; 
+				
+			//	echo $output;
+			?>
+			<?php 
+			if($this->uri->segment(4)== null){
+					echo "";
+				}else if($this->uri->segment(4)== !null){
+				
+			?>
+			<div id="contact_form">
+			<?php echo form_open('ecommerce/createThread/'.$this->uri->segment(4)); ?>
+						<input type="hidden" id="author" name="id" value="<?php echo $this->uri->segment(4);?>"/>
+						
+						<label for="author">Title:</label> <input type="text" id="author" name="title" class="required input_field" />
+						<div class="cleaner h10"></div>
+						
+						<label for="comments">Body:</label> 
+						<textarea name="comments" id="comments" rows="0" cols="0" class="required"></textarea>
+						<div class="cleaner h10"></div>
+												
+						<input type="submit" value="Send" id="submit" name="submit" class="submit_btn float_l" />
+						<input type="reset" value="Reset" id="reset" name="reset" class="submit_btn float_r" />
+						
+					</form>
+				</div>
+			<?php }  ?>
             
         </div> <!-- END of content -->
         <div class="cleaner"></div>
